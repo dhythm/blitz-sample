dd915ec41b6ecdec39b15a0a51760a341f4561ba
dhythm <yuta.okada.20@gmail.com>
2022-01-07 19:49:54 +0900

    wip

--- app/pages/index.test.tsx
+++ app/pages/index.test.tsx
@@ -9,8 +9,8 @@ import Home from "./index"
 jest.mock("app/users/queries/getCurrentUser")
 const mockUseGetUser = getCurrentUser as jest.MockedFunction<typeof getCurrentUser>
 
+// test.skip("renders blitz documentation link", () => {
 test("renders blitz documentation link", () => {
-  // test.skip("renders blitz documentation link", () => {
   // This is an example of how to ensure a specific item is in the document
   // But it's disabled by default (by test.skip) so the test doesn't fail
   // when you remove the the default content from the page
