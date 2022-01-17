1e3f2173de264f242d976a1c7c89909c7e99475d
dhythm <yuta.okada.20@gmail.com>
2022-01-17 10:53:43 +0900

    update

--- app/core/hooks/useCurrentUser2.ts
+++
@@ -1,7 +0,0 @@
-import getCurrentUser from "app/rpc/user2/queries/getCurrentUser"
-import { useQuery } from "blitz"
-
-export const useCurrentUser = () => {
-  const [user] = useQuery(getCurrentUser, null)
-  return user
-}
