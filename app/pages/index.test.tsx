00648237a0aa9de0dedef7bf19071c7825479741
dhythm <yuta.okada.20@gmail.com>
2022-01-17 10:55:15 +0900

    update

--- app/pages/index.test.tsx
+++ app/pages/index.test.tsx
@@ -5,7 +5,7 @@ import Home from "./index"
 
 jest.mock("blitz", () => ({
   ...jest.requireActual<any>("blitz"),
-  useQuery: jest.fn(),
+  // useQuery: jest.fn(),
   Routes: {
     SignupPage: jest.fn().mockReturnValue(""),
     LoginPage: jest.fn().mockReturnValue(""),
@@ -28,19 +28,19 @@ test("renders blitz documentation link", () => {
   // But it's disabled by default (by test.skip) so the test doesn't fail
   // when you remove the the default content from the page
 
-  mockUseQuery.mockReturnValue([
-    {
-      id: 1,
-      name: "User",
-      email: "user@email.com",
-      role: "user",
-    },
-    {
-      // refetch: jest.fn(() => {
-      //   return Promise.resolve({ id: 1, name: "User", email: "user@email.com", role: "user" })
-      // }),
-    } as any,
-  ])
+  // mockUseQuery.mockReturnValue([
+  //   {
+  //     id: 1,
+  //     name: "User",
+  //     email: "user@email.com",
+  //     role: "user",
+  //   },
+  //   {
+  //     // refetch: jest.fn(() => {
+  //     //   return Promise.resolve({ id: 1, name: "User", email: "user@email.com", role: "user" })
+  //     // }),
+  //   } as any,
+  // ])
   // This is an example on how to mock api hooks when testing
   // mockUseCurrentUser.mockReturnValue({
   //   id: 1,
