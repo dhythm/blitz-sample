88006a500f71006bde594609c9f09c3e0f0b5c9b
dhythm <yuta.okada.20@gmail.com>
2022-02-11 11:36:25 +0900

    update

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from "@jest/types"
+import preset from "jest-preset"
 
 const config: Config.InitialOptions = {
   preset: "blitz",
+  coverageThreshold: preset.coverageThreshold,
 }
 
 export default config
